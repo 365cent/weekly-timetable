90b6f5e170b7f1e7e392752021cc71c88873a41b
365cent <74791570+365cent@users.noreply.github.com>
2024-09-04 23:30:50 -0400

    fix: Refactor courseData.ts to use TypeScript types

--- src/lib/courseData.ts
+++ src/lib/courseData.ts
@@ -1,135 +1,154 @@
-export const courseData = {
+type Schedule = {
+    day: string
+    startTime: string
+    endTime: string
+    place: string
+}
+
+type Course = {
+    code: string
+    alias: string
+    title: string
+    instructor: string
+    schedule: Schedule[]
+}
+
+type CourseData = {
+    courses: Course[]
+}
+
+export const courseData : CourseData = {
     "courses": [
-      {
-        "code": "CS9511",
-        "alias": "CS4482",
-        "title": "Game Programming",
-        "instructor": "",
-        "schedule": [
-          { "day": "Monday", "startTime": "12:30", "endTime": "14:30", "place": "HSB 236" },
-          { "day": "Wednesday", "startTime": "13:30", "endTime": "14:30", "place": "HSB 236" }
-        ]
-      },
-      {
-        "code": "CS9538",
-        "alias": "CS4411",
-        "title": "Databases II",
-        "instructor": "Mostafa Milani",
-        "schedule": [
-          { "day": "Tuesday", "startTime": "12:30", "endTime": "14:30", "place": "HSB 236" },
-          { "day": "Thursday", "startTime": "12:30", "endTime": "13:30", "place": "HSB 236" }
-        ]
-      },
-      {
-        "code": "CS9542",
-        "alias": "CS4442",
-        "title": "Artificial Intelligence II",
-        "instructor": "Wang/Mohsenzadeh",
-        "schedule": [
-          { "day": "Monday", "startTime": "09:30", "endTime": "11:30", "place": "NCB 113" },
-          { "day": "Wednesday", "startTime": "10:30", "endTime": "11:30", "place": "NCB 113" }
-        ]
-      },
-      {
-        "code": "CS9549",
-        "alias": "CS4471",
-        "title": "Software Design & Architecture",
-        "instructor": "Nazim Madhavji",
-        "schedule": [
-          { "day": "Friday", "startTime": "10:30", "endTime": "13:30", "place": "AHB 1B08" }
-        ]
-      },
-      {
-        "code": "CS9628",
-        "alias": "CS4481",
-        "title": "Image Compression",
-        "instructor": "Mahmoud El-Sakka",
-        "schedule": [
-          { "day": "Wednesday", "startTime": "10:30", "endTime": "13:30", "place": "MC 105B" }
-        ]
-      },
-      {
-        "code": "CS9636",
-        "alias": "CS4458",
-        "title": "Network Security",
-        "instructor": "Anwar Haque",
-        "schedule": [
-          { "day": "Tuesday", "startTime": "09:30", "endTime": "12:30", "place": "SEB 2202" }
-        ]
-      },
-      {
-        "code": "CS9639",
-        "alias": "",
-        "title": "Information Visualization",
-        "instructor": "Kamran Sedig",
-        "schedule": [
-          { "day": "Monday", "startTime": "13:30", "endTime": "16:30", "place": "MC 320" }
-        ]
-      },
-      {
-        "code": "CS9642",
-        "alias": "CS2120",
-        "title": "Programming for Scientists",
-        "instructor": "",
-        "schedule": [
-          { "day": "Tuesday", "startTime": "10:30", "endTime": "12:30", "place": "UCC 146" },
-          { "day": "Thursday", "startTime": "13:30", "endTime": "14:30", "place": "UCC 146" }
-        ]
-      },
-      {
-        "code": "CS9544",
-        "alias": "CS4445",
-        "title": "Analysis of Algorithms II",
-        "instructor": "Roberto Solis-Oba",
-        "schedule": [
-          { "day": "Friday", "startTime": "13:30", "endTime": "15:30", "place": "SSC 2028" }
-        ]
-      },
-      {
-        "code": "CS9832",
-        "alias": "",
-        "title": "Topics in Bioinformatics",
-        "instructor": "Kaizhong Zhang",
-        "schedule": [
-          { "day": "Tuesday", "startTime": "14:30", "endTime": "16:30", "place": "MC 320" }
-        ]
-      },
-      {
-        "code": "CS9866",
-        "alias": "",
-        "title": "Topics in Data Management",
-        "instructor": "Mostafa Milani",
-        "schedule": [
-          { "day": "Tuesday", "startTime": "09:30", "endTime": "11:30", "place": "MC 320" },
-          { "day": "Thursday", "startTime": "10:30", "endTime": "11:30", "place": "MC 320" }
-        ]
-      },
-      {
-        "code": "CS9875",
-        "alias": "",
-        "title": "Theoretical Machine Learning",
-        "instructor": "Boyu Wang",
-        "schedule": [
-          { "day": "Friday", "startTime": "13:30", "endTime": "15:30", "place": "MC 320" }
-        ]
-      },
-      {
-        "code": "CS9877",
-        "alias": "CS4463",
-        "title": "Computational Biology",
-        "instructor": "Lucian Ilie",
-        "schedule": [
-          { "day": "Wednesday", "startTime": "15:30", "endTime": "17:30", "place": "PAB 36" }
-        ]
-      },
-      {
-        "code": "DS9000",
-        "alias": "",
-        "title": "Intro to Machine Learning",
-        "instructor": "Daniel Lizotte",
-        "schedule": [
-          { "day": "Tuesday", "startTime": "15:30", "endTime": "17:30", "place": "NSC 7" }
-        ]
-      }
+        {
+            "code": "CS9511",
+            "alias": "CS4482",
+            "title": "Game Programming",
+            "instructor": "",
+            "schedule": [
+                { "day": "Monday", "startTime": "12:30", "endTime": "14:30", "place": "HSB 236" },
+                { "day": "Wednesday", "startTime": "13:30", "endTime": "14:30", "place": "HSB 236" }
+            ]
+        },
+        {
+            "code": "CS9538",
+            "alias": "CS4411",
+            "title": "Databases II",
+            "instructor": "Mostafa Milani",
+            "schedule": [
+                { "day": "Tuesday", "startTime": "12:30", "endTime": "14:30", "place": "HSB 236" },
+                { "day": "Thursday", "startTime": "12:30", "endTime": "13:30", "place": "HSB 236" }
+            ]
+        },
+        {
+            "code": "CS9542",
+            "alias": "CS4442",
+            "title": "Artificial Intelligence II",
+            "instructor": "Wang/Mohsenzadeh",
+            "schedule": [
+                { "day": "Monday", "startTime": "09:30", "endTime": "11:30", "place": "NCB 113" },
+                { "day": "Wednesday", "startTime": "10:30", "endTime": "11:30", "place": "NCB 113" }
+            ]
+        },
+        {
+            "code": "CS9549",
+            "alias": "CS4471",
+            "title": "Software Design & Architecture",
+            "instructor": "Nazim Madhavji",
+            "schedule": [
+                { "day": "Friday", "startTime": "10:30", "endTime": "13:30", "place": "AHB 1B08" }
+            ]
+        },
+        {
+            "code": "CS9628",
+            "alias": "CS4481",
+            "title": "Image Compression",
+            "instructor": "Mahmoud El-Sakka",
+            "schedule": [
+                { "day": "Wednesday", "startTime": "10:30", "endTime": "13:30", "place": "MC 105B" }
+            ]
+        },
+        {
+            "code": "CS9636",
+            "alias": "CS4458",
+            "title": "Network Security",
+            "instructor": "Anwar Haque",
+            "schedule": [
+                { "day": "Tuesday", "startTime": "09:30", "endTime": "12:30", "place": "SEB 2202" }
+            ]
+        },
+        {
+            "code": "CS9639",
+            "alias": "",
+            "title": "Information Visualization",
+            "instructor": "Kamran Sedig",
+            "schedule": [
+                { "day": "Monday", "startTime": "13:30", "endTime": "16:30", "place": "MC 320" }
+            ]
+        },
+        {
+            "code": "CS9642",
+            "alias": "CS2120",
+            "title": "Programming for Scientists",
+            "instructor": "",
+            "schedule": [
+                { "day": "Tuesday", "startTime": "10:30", "endTime": "12:30", "place": "UCC 146" },
+                { "day": "Thursday", "startTime": "13:30", "endTime": "14:30", "place": "UCC 146" }
+            ]
+        },
+        {
+            "code": "CS9544",
+            "alias": "CS4445",
+            "title": "Analysis of Algorithms II",
+            "instructor": "Roberto Solis-Oba",
+            "schedule": [
+                { "day": "Friday", "startTime": "13:30", "endTime": "15:30", "place": "SSC 2028" }
+            ]
+        },
+        {
+            "code": "CS9832",
+            "alias": "",
+            "title": "Topics in Bioinformatics",
+            "instructor": "Kaizhong Zhang",
+            "schedule": [
+                { "day": "Tuesday", "startTime": "14:30", "endTime": "16:30", "place": "MC 320" }
+            ]
+        },
+        {
+            "code": "CS9866",
+            "alias": "",
+            "title": "Topics in Data Management",
+            "instructor": "Mostafa Milani",
+            "schedule": [
+                { "day": "Tuesday", "startTime": "09:30", "endTime": "11:30", "place": "MC 320" },
+                { "day": "Thursday", "startTime": "10:30", "endTime": "11:30", "place": "MC 320" }
+            ]
+        },
+        {
+            "code": "CS9875",
+            "alias": "",
+            "title": "Theoretical Machine Learning",
+            "instructor": "Boyu Wang",
+            "schedule": [
+                { "day": "Friday", "startTime": "13:30", "endTime": "15:30", "place": "MC 320" }
+            ]
+        },
+        {
+            "code": "CS9877",
+            "alias": "CS4463",
+            "title": "Computational Biology",
+            "instructor": "Lucian Ilie",
+            "schedule": [
+                { "day": "Wednesday", "startTime": "15:30", "endTime": "17:30", "place": "PAB 36" }
+            ]
+        },
+        {
+            "code": "DS9000",
+            "alias": "",
+            "title": "Intro to Machine Learning",
+            "instructor": "Daniel Lizotte",
+            "schedule": [
+                { "day": "Tuesday", "startTime": "15:30", "endTime": "17:30", "place": "NSC 7" }
+            ]
+        }
     ]
-  }
+}
